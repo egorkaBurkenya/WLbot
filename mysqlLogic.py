4e93430ff13b2ecd60b38eb0f606dcecfda179e4
egorkaBurkenya <sleepyegorka@gmail.com>
2021-01-11 01:10:20 +0300

    some commit

--- mysqlLogic.py
+++ mysqlLogic.py
@@ -17,7 +17,7 @@ def add_user(user_id):
 def add_new_value(user_id, position, value):
 	table = Tables('users')
 	if position == 1:
-		table.insert('first_value', f'"{value}"')
+		table.update(f'first_value = "{value}"', f'id = "{user_id}"')
 	if position == 2:
 		table.update('second_value', f'"{value}"')
 
