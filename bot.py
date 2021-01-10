559bcc67d598d8270213944a0001fc5bbc1b5e4b
egorkaBurkenya <sleepyegorka@gmail.com>
2021-01-10 19:50:53 +0300

    some commit

--- bot.py
+++ bot.py
@@ -6,38 +6,38 @@ from loguru import logger
 from mysqlLogic import *
 
 with open('config.json', 'r', encoding='utf-8') as fh:
-  config = json.load(fh)
+	config = json.load(fh)
 
 bot = telebot.TeleBot(config['TOKEN'])
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-  add_user(message.chat.id)
-  markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-  item1 = types.KeyboardButton("How to use")
-  item2 = types.KeyboardButton("Start learning")
-  markup.add(item1, item2)
-  bot.send_message(message.chat.id, 'Hello boy!', reply_markup=markup)
+	add_user(message.chat.id)
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	item1 = types.KeyboardButton("How to use")
+	item2 = types.KeyboardButton("Start learning")
+	markup.add(item1, item2)
+	bot.send_message(message.chat.id, 'Hello boy!', reply_markup=markup)
 
 @bot.message_handler(commands=['createDB'])
 def create_new_db(message):
-  create_table()
-  bot.send_message(message.chat.id, 'create ! ')
+  	create_table()
+  	bot.send_message(message.chat.id, 'create ! ')
 
 
 @bot.message_handler(content_types=['text'])
 def lisener(message):
-	if message.chat.type == 'private':
-		if message.text == 'How to use':
+    if message.chat.type == 'private':
+        if message.text == 'How to use':
 			bot.send_message(message.chat.id, 'First of all, click "Start learning" to start your learning process')
-		if message.text == 'Start learning':
-                        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-                        item1 = types.KeyboardButton("/addNew")
-                        item2 = types.KeyboardButton("/handleCheck")
-                        item3 = types.KeyboardButton("/seeAll")
-                        item4 = types.KeyboardButton("/delete")
-                        markup.add(item1, item2, item3, item4)
-		        bot.send_message(message.chat.id, 'popaka', reply_markup=markup)
+        if message.text == 'Start learning':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            item1 = types.KeyboardButton("/addNew")
+            item2 = types.KeyboardButton("/handleCheck")
+            item3 = types.KeyboardButton("/seeAll")
+            item4 = types.KeyboardButton("/delete")
+            markup.add(item1, item2, item3, item4)
+            bot.send_message(message.chat.id, 'popaka', reply_markup=markup)
 
 bot.polling(none_stop=True)
 
