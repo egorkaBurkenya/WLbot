1a871c12377a56a96d07ae46823f846b1b7b760c
egorkaBurkenya <sleepyegorka@gmail.com>
2021-01-11 04:18:01 +0300

    some commit

--- func.py
+++ func.py
@@ -2,6 +2,6 @@ def unpucking(value):
   answer = ''
   num = 0
   for i in value:
-````num += 1
+    num += 1
     answer += f'\n {num}. {i["first_value"]} - {i["second_value"]}\n_'
   return answer
